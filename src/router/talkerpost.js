18a30ae9aead1778dd04eac66faf9f8b443b5486
BenHur Albertassi da Silva <benhuralber@yahoo.com.br>
2022-12-16 12:27:44 -0300

    Fix:  Create Routes

--- src/router/talkerpost.js
+++ src/router/talkerpost.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const { fieldAge } = require('../validator/fieldAge');
 const { fieldName } = require('../validator/fieldName');
 const { fieldRate } = require('../validator/fieldRate');
-const { fieldTalk } = require('../validator/fieldTalk');
+const { fieldTalk } = require('../validator/fieldWatched');
+const { fieldWatched } = require('../validator/fieldWatched');
 
 module.exports = router;
