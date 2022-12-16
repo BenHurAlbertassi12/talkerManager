c9fdb172d6c2ebe71ba2c423a1dd0b7fe8f5f543
BenHur Albertassi da Silva <benhuralber@yahoo.com.br>
2022-12-16 12:08:32 -0300

    Fix: Create Validation Talk

--- src/validator/fieldTalk.js
+++ src/validator/fieldTalk.js
@@ -1,10 +1,5 @@
 module.exports = (req, res, next) => {
-    const { age } = req.body;
-    if (!age) {
-        res.status(400).json({ message: 'O campo "age" é obrigatório' });
-    }
-    if (age <= 17) {
-        res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
-    }
+    const { talk } = req.body;
+    if (!talk) { res.status(400).json({ message: 'O campo "talk" é obrigatório' }); }
     next();
 };
