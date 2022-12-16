10a293765d513ffe708ef066d9a0b3ccc84f82dc
BenHur Albertassi da Silva <benhuralber@yahoo.com.br>
2022-12-16 12:08:56 -0300

    Fix: Create Validation Watched

---
+++ src/validator/fieldWatched.js
@@ -0,0 +1,16 @@
+module.exports = (req, res, next) => {
+    const { talk: { watchedAt } } = req.body;
+   
+    if (!watchedAt) {
+        res.status(400).json({ message: 'O campo "watchedAt" é obrigatório' });
+    }
+
+    const isFormatDate = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+
+    if (!isFormatDate.test(watchedAt)) {
+        res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+    }
+    next();
+};
+
+// regex da data retirado do gabarito do dia 04
