b3721d2d419a5879c2c868cb9923e1c8947e3648
BenHur Albertassi da Silva <benhuralber@yahoo.com.br>
2022-12-16 11:44:14 -0300

    Fix: Importation Fields

--- src/router/talkerpost.js
+++ src/router/talkerpost.js
@@ -1,3 +1,10 @@
 const express = require('express');
 
 const router = express.Router();
+
+const { fieldAge } = require('../validator/fieldAge');
+const { fieldName } = require('../validator/fieldName');
+const { fieldRate } = require('../validator/fieldRate');
+const { fieldTalk } = require('../validator/fieldTalk');
+
+module.exports = router;
